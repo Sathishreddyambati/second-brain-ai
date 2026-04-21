d8df8d8172f924a41b32da6d31f80110ceef7761
Supreme Hero Sathi <122785736+Sathishreddyambati@users.noreply.github.com>
2026-04-21 12:10:46 +0000

    Import Firebase modules for Firestore usage

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 const user = JSON.parse(localStorage.getItem("user"));
 
 if (!user) {
